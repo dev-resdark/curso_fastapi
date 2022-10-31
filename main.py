ac618ca38eeed61c255138ca2c48748aa66493a0
sergio Resq <giodaquinba@gmail.com>
2022-10-31 20:44:54 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from webbrowser import get
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/") #esto es un decodorador de la funcion que vamos a crear
+
+def home():
+    return{"Hello": "World"}
